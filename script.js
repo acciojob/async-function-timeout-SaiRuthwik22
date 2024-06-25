f94bd45c526c42c9ed8211f4d30379aaea8818f6
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-25 07:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function myPromise(delay) {
+	
+	return new Promise((resolve,reject)=>{
+		setTimeout(display,delay)
+		 resolve()
+		})
+}
+function display() {
+	let text = document.getElementById("text").value
+	let output = document.getElementById("output")
+	output.textContent = text
+}
+async function myFunc(event) {
+	event.preventDefault()
+	let delay = document.getElementById("delay").value
+	await myPromise(delay)
+}
+	
+}
