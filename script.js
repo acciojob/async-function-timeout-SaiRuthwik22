75f4de964afb07e8e0316bbaf96c69fcfe9deb74
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-25 08:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your JS code here. If required.
 function myPromise(delay) {
-	let text = document.getElementById("text").value
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(text);
+            resolve();
         }, delay)
     })
 }
 
 async function myFunc(event) {
     event.preventDefault();
-    let delay = document.getElementById("delay").value;
-    let text1 = await myPromise(delay);
+	let text = document.getElementById("text").value
+    let delay = Number(document.getElementById("delay").value);
+    await myPromise(delay);
     let output = document.getElementById("output");
-    output.textContent = text1;
+    output.textContent = text;
 }
